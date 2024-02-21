5bbf16b731e8350267e44d049095dec4c0a4871f
pc <rahimjangi1982@gmail.com>
2024-02-21 19:49:19 -0400

    Navbar added

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PetShop';
+  products: Product[] = [];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<Pagination<Product[]>>('https://localhost:5001/api/Products')
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          this.products = response.data;
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
 }
