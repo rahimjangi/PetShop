f49ec516b88ce9860fa0a1d0269cb227a24f89ba
pc <rahimjangi1982@gmail.com>
2024-02-22 23:54:20 -0400

    Pagination added!

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -13,11 +13,18 @@ namespace API.Controllers;
 public class ProductsController : BaseApiController
 {
     private readonly IGenericRepository<Product> _productRepository;
+    private readonly IGenericRepository<ProductBrand> _brandReoisitory;
+    private readonly IGenericRepository<ProductType> _typeReoisitory;
     private readonly IMapper _mapper;
 
-    public ProductsController(IGenericRepository<Product> productRepository, IMapper mapper)
+    public ProductsController(IGenericRepository<Product> productRepository,
+        IGenericRepository<ProductBrand> brandReoisitory,
+        IGenericRepository<ProductType> typeReoisitory,
+        IMapper mapper)
     {
         _productRepository = productRepository;
+        _brandReoisitory = brandReoisitory;
+        _typeReoisitory = typeReoisitory;
         _mapper = mapper;
     }
     [HttpGet]
@@ -40,5 +47,17 @@ public class ProductsController : BaseApiController
         var product = await _productRepository.GetEntityWithSpec(spec);
         if (product == null) { return NotFound(new ApiResponse(404)); }
         return _mapper.Map<Product, ProductToReturnDto>(product);
+    }
+
+    [HttpGet("brands")]
+    public async Task<IReadOnlyList<ProductBrand>> GetBrands()
+    {
+        return await _brandReoisitory.ListAllAsync();
+    }
+
+    [HttpGet("types")]
+    public async Task<IReadOnlyList<ProductType>> GetTypes()
+    {
+        return await _typeReoisitory.ListAllAsync();
     }
 }
--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from './models/product';
-import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,20 +7,7 @@ import { Pagination } from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'PetShop';
-  products: Product[] = [];
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.http
-      .get<Pagination<Product[]>>('https://localhost:5001/api/Products')
-      .subscribe({
-        next: (response) => {
-          console.log(response);
-          this.products = response.data;
-        },
-        error: (err) => {
-          console.log(err);
-        },
-      });
-  }
+  ngOnInit(): void {}
 }
